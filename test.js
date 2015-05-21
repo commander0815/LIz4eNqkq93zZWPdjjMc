03f3d494370a06fe59f4e65817b6df6987c88672
commander0815 <sinep3@gmx.de>
2015-05-21 17:15:55 +0200

    Bugs:
    - menu bleibt nicht oben (zu dumm für java script, kot ausgelagert in test.js)
    - menü rechts rund oder komplett eckig?
    - dropdown grüner balken unten (rules)
    - searchbutton Farbe?
    - searchfeld noch kleiner, Farbe?
    
    todo:
    - slideshow über dem Menü (Größe noch anpassen)
    - Gallery design
    - news seite [jambit like, ....war ja nicht alles schlecht xD)

---
+++ test.js
@@ -0,0 +1,17 @@
+$('#nav').affix({
+});
+
+
+
+
+css:
+#nav.affix {
+    position: fixed;
+    top: 0;
+    width: 100%
+}
+
+
+html:
+navbar container
+<div class="container-fluid" data-spy="affix" data-offset-top="400" id="nav"
