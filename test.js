a32ea82ff4c1f60e746968a5ec75640d8b5d3e37
commander0815 <sinep3@gmx.de>
2015-05-22 17:02:37 +0200

    menu bug fixed, divider fixed

--- test.js
+++ test.js
@@ -1,17 +1,11 @@
 $('#nav').affix({
-});
+      offset: {
+        top: $('header').height()
+      }
+});	
 
-
-
-
-css:
-#nav.affix {
-    position: fixed;
-    top: 0;
-    width: 100%
-}
-
-
-html:
-navbar container
-<div class="container-fluid" data-spy="affix" data-offset-top="400" id="nav"
+$('#sidebar').affix({
+      offset: {
+        top: 200
+      }
+});	
